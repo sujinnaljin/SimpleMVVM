e02fc139e0eba95420e046cc1a956e4e20a65fa0
Seungwook-Jung <int0124@naver.com>
2020-11-07 19:08:26 +0900

    Initial Commit

---
+++ simpleMVVM/simpleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleMVVMApp.swift
+//  simpleMVVM
+//
+//  Created by seungwook.jung on 2020/11/07.
+//
+
+import SwiftUI
+
+@main
+struct simpleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
